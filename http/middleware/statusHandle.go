5893c1ee9c3bd431c0f9a248403831b1f19963b0
lihuanjie <lihuanjie113@gmail.com>
2023-04-03 14:40:01 +0800

    fixed

--- http/middleware/statusHandle.go
+++ http/middleware/statusHandle.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"tutu-gin/core/api"
@@ -9,8 +10,9 @@ import (
 
 func StatusHandle() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
+		fmt.Println("本地解析客户端地址：" + c.ClientIP())
 		if c.ClientIP() == "118.25.251.62" {
+			fmt.Println("检测到异常ip地址")
 			c.JSON(http.StatusOK, api.ApiSuccessResponse(parserDto.ParserResultDto{
 				Title:     "请关注微信公众号『考拉解析』进行视频去水印",
 				CoverUrls: "https://img.alicdn.com/bao/uploaded/i1/O1CN01MVPP541Pyp5ooFMjU_!!2-rate.png",
