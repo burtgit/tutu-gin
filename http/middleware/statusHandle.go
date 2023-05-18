255a07e2d9c0583681c7c1c0350921f156101de0
lihuanjie <lihuanjie113@gmail.com>
2023-05-18 21:57:21 +0800

    fixed

--- http/middleware/statusHandle.go
+++ http/middleware/statusHandle.go
@@ -14,7 +14,7 @@ func StatusHandle() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ip := c.ClientIP()
 		fmt.Println("本地解析客户端地址：" + ip)
-		if ip == "118.25.251.62" || ip == "119.29.94.101" {
+		if ip == "118.25.251.62" || ip == "119.29.94.101" || ip == "101.36.127.200" {
 			fmt.Println("检测到异常ip地址")
 			c.JSON(http.StatusOK, api.ApiSuccessResponse(parserDto.ParserResultDto{
 				Title:     "请关注微信公众号『考拉解析』进行视频去水印",
