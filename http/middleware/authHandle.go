b7e3156d292f4ed3fa151d4c83618a678afd3dbb
lihuanjie <lihuanjie113@gmail.com>
2023-09-02 20:20:54 +0800

    fixed

--- http/middleware/authHandle.go
+++ http/middleware/authHandle.go
@@ -42,7 +42,7 @@ func AuthHandle() gin.HandlerFunc {
 
 		if userInfo.Status == 0 {
 			c.JSON(http.StatusOK, api.ApiSuccessResponse(parserDto.ParserResultDto{
-				Title:     "请关注微信公众号『考拉解析』进行视频去水印",
+				Title:     "请关注微信公众号『大拿解析』进行视频去水印",
 				CoverUrls: "https://img.alicdn.com/bao/uploaded/i1/O1CN01MVPP541Pyp5ooFMjU_!!2-rate.png",
 				VideoUrls: "http://gslb.miaopai.com/stream/JHeJmOV8InV6MISxrZAD8xsps4Mga0-IxSTERw__.mp4?vend=miaopai&ssig=6c6f8ac141464a5c6244c0cf7b9751a5&time_stamp=1592927773813&mpflag=32&unique_id=1592924158668318",
 				IsVideo:   true,
