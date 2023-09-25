7074043ab0c8bed3623755095a0718398400e9cd
lihuanjie <lihuanjie113@gmail.com>
2023-09-25 10:59:39 +0800

    fixed

--- http/web/index.go
+++ http/web/index.go
@@ -10,7 +10,7 @@ import (
 type Index struct{}
 
 func (i Index) Index(c *gin.Context) {
-	if !strings.Contains(c.Request.Host, "danajx.com") {
+	if strings.Contains(c.Request.Host, "danajx.com") {
 		c.HTML(http.StatusOK, "dana_index.html", nil)
 	} else {
 		c.HTML(http.StatusOK, "index.html", nil)
