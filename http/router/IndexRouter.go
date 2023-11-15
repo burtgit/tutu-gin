2be521007ea6ea59d3e9fe6a4c1e1f484508599f
lihuanjie <lihuanjie113@gmail.com>
2023-11-15 12:14:22 +0800

    fixed

--- http/router/IndexRouter.go
+++ http/router/IndexRouter.go
@@ -10,4 +10,12 @@ type IndexRouter struct{}
 func (i IndexRouter) Init(router *gin.Engine) {
 	indexController := web.NewIndex()
 	router.GET("/", indexController.Index)
+	router.GET("/xiaohongshu.html", indexController.Index)
+	router.GET("/weibo.html", indexController.Index)
+	router.GET("/xigua.html", indexController.Index)
+	router.GET("/kuaishou.html", indexController.Index)
+	router.GET("/bilibili.html", indexController.Index)
+	router.GET("/instagram.html", indexController.Index)
+	router.GET("/youtube.html", indexController.Index)
+	router.GET("/tiktok.html", indexController.Index)
 }
--- http/web/index.go
+++ http/web/index.go
@@ -11,7 +11,50 @@ type Index struct{}
 
 func (i Index) Index(c *gin.Context) {
 	if strings.Contains(c.Request.Host, "danaqsy.com") {
-		c.HTML(http.StatusOK, "dana_index.html", nil)
+
+		title := "抖音"
+		example := "http://v.douyin.com/22TnGD/"
+		desc := "抖音 视频"
+		path := c.Request.RequestURI
+		if c.Request.RequestURI == "/xiaohongshu.html" {
+			title = "小红书"
+			example = "http://xhslink.com/hlVaar"
+			desc = "小红书 视频/图片"
+		} else if c.Request.RequestURI == "/kuaishou.html" {
+			title = "快手"
+			example = "https://v.kuaishou.com/ruZDM9"
+			desc = "快手 视频"
+		} else if c.Request.RequestURI == "/xigua.html" {
+			title = "西瓜"
+			example = "https://www.ixigua.com/7223945085413818880"
+			desc = "西瓜 视频"
+		} else if c.Request.RequestURI == "/weibo.html" {
+			title = "微博"
+			example = "http://m.weibo.cn/status/4885315180037721?"
+			desc = "微博 视频"
+		} else if c.Request.RequestURI == "/bilibili.html" {
+			title = "哔哩哔哩"
+			example = "https://www.bilibili.com/video/BV1WS4y1H7yG"
+			desc = "哔哩哔哩 视频"
+		} else if c.Request.RequestURI == "/instagram.html" {
+			title = "Instagram"
+			example = "https://www.instagram.com/p/Ce1wWSQrSua/"
+			desc = "ins图片/视频"
+		} else if c.Request.RequestURI == "/youtube.html" {
+			title = "YouTube"
+			example = "https://www.youtube.com/watch?v=UTKS3UKUCUs/"
+			desc = "YouTube 视频"
+		} else if c.Request.RequestURI == "/tiktok.html" {
+			title = "Tiktok"
+			example = "https://www.tiktok.com/t/ZPRvFSKCn/"
+			desc = "Tiktok 视频"
+		}
+		c.HTML(http.StatusOK, "dana_index.html", gin.H{
+			"title":   title,
+			"example": example,
+			"desc":    desc,
+			"path":    path,
+		})
 	} else {
 		c.HTML(http.StatusOK, "index.html", nil)
 	}
